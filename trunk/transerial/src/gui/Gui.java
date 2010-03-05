6a40d65b70b35aaab63bc05cdf7a8a4507c0e7d8
pedroveras <pedroveras@cc2da39a-27b5-11df-959e-df69dd781d82>
2010-03-05 05:04:54 +0000

    Incluído outra caixa de texto, sendo uma para envio e outra recepção os dados. incluído botão enviar e botão conecta

--- trunk/transerial/src/gui/Gui.java
+++ trunk/transerial/src/gui/Gui.java
@@ -11,8 +11,16 @@
 
 package gui;
 
-import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
 
+import com.sun.java.swing.plaf.nimbus.ToggleButtonPainter;
+
+import acao.Play;
+
+import util.Portas;
 /**
  *
  * @author arce
@@ -34,6 +42,7 @@ public class Gui extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
+        jScrollPane2 = new javax.swing.JScrollPane();
         areaDados = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
         tamAreaDado = new javax.swing.JTextField();
@@ -49,21 +58,34 @@ public class Gui extends javax.swing.JFrame {
         stopBit = new javax.swing.JComboBox();
         jCheckBox1 = new javax.swing.JCheckBox();
         jBtEnviar = new javax.swing.JButton();
+        btnConecta = new JToggleButton("Conecta");
         dest = new javax.swing.JCheckBox();
+        portas = new Portas();
+        recepcao = new JTextArea();
+        envio = new JTextArea();
+        labelEnvio = new JLabel();
+        labelRecepcao = new JLabel();
+        play = new Play();
 
+        
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Redes I - 2010.1");
         setResizable(false);
 
-        areaDados.setColumns(20);
-        areaDados.setRows(5);
-        jScrollPane1.setViewportView(areaDados);
+        envio.setColumns(20);
+        envio.setRows(5);
+        jScrollPane1.setViewportView(envio);
+        
+        recepcao.setColumns(20);
+        recepcao.setRows(5);        
+        recepcao.setEditable(false);
+        jScrollPane2.setViewportView(recepcao);
 
         jLabel1.setText("Tamanho da área de dados");
 
         jLabel2.setText("Porta de Comunicação");
 
-        destino.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "COM 1", "COM 2" }));
+        destino.setModel(new javax.swing.DefaultComboBoxModel(portas.getPortas()));
 
         jLabel3.setText("Taxa de transmissão");
 
@@ -78,12 +100,20 @@ public class Gui extends javax.swing.JFrame {
         stopBit.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "1,5", "2" }));
 
         jCheckBox1.setText("Simulação de erro?");
+        labelEnvio.setText("Envio:");
+        labelRecepcao.setText("Recep��o:");
 
         jBtEnviar.setText("Enviar");
         jBtEnviar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBtEnviarActionPerformed(evt);
             }
+        });
+        
+        btnConecta.addActionListener(new java.awt.event.ActionListener() {
+        	public void actionPerformed(java.awt.event.ActionEvent evt) {
+        		btnConectaActionPerformed(evt);
+        	}
         });
 
         dest.setText("Estação destino ?");
@@ -100,7 +130,11 @@ public class Gui extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                	.addComponent(labelEnvio)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 555, Short.MAX_VALUE)
+                    .addComponent(jBtEnviar)
+                    .addComponent(labelRecepcao)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 555, Short.MAX_VALUE)
                     .addComponent(jLabel5)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -124,7 +158,7 @@ public class Gui extends javax.swing.JFrame {
                         .addGap(102, 102, 102)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(jCheckBox1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jBtEnviar)
+                            .addComponent(btnConecta)
                             .addComponent(dest, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap())
         );
@@ -132,7 +166,11 @@ public class Gui extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(32, 32, 32)
+                .addComponent(labelEnvio, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jBtEnviar, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(labelRecepcao, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
@@ -159,21 +197,32 @@ public class Gui extends javax.swing.JFrame {
                     .addComponent(dest))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel6)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(stopBit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jBtEnviar)))
-                .addContainerGap())
+                        .addComponent(jLabel6)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(stopBit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnConecta)))
+                    .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jBtEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtEnviarActionPerformed
-        // TODO add your handling code here:
         System.out.println("Começa aqui!!!!");
 
     }//GEN-LAST:event_jBtEnviarActionPerformed
+    
+    private void btnConectaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtEnviarActionPerformed
+    	if (btnConecta.isSelected()){
+    		btnConecta.setText("Desconecta");
+    		play.conecta(destino.getSelectedItem().toString());
+    	}
+    	else
+    	{
+    		btnConecta.setText("Conecta");
+    		play.desconecta();
+    	}
+    }
 
     private void destActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_destActionPerformed
         // TODO add your handling code here:
@@ -196,6 +245,7 @@ public class Gui extends javax.swing.JFrame {
     private javax.swing.JCheckBox dest;
     private javax.swing.JComboBox destino;
     private javax.swing.JButton jBtEnviar;
+    private javax.swing.JToggleButton btnConecta;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -203,12 +253,19 @@ public class Gui extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel labelEnvio;
+    private javax.swing.JLabel labelRecepcao;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private Portas portas;
     private javax.swing.JComboBox paridade;
     private javax.swing.JComboBox stopBit;
     private javax.swing.JTextField tamAreaDado;
     private javax.swing.JTextField tamPalavra;
     private javax.swing.JTextField taxaTrans;
+    private JTextArea envio;
+    private JTextArea recepcao;
+    private Play play;
     // End of variables declaration//GEN-END:variables
 
 }
